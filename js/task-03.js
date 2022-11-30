3e5aa0788bab3f86e9c9eecd23feea3876b7d310
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-11-30 21:07:05 +0200

    Make task-3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,12 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery');
+
+const markup = images.map(({url, alt}) => 
+`<li class="list">
+<img class="images" src = "${url}" alt = "${alt}">
+</li>`).join('');
+
+list.insertAdjacentHTML('beforeend', markup);
