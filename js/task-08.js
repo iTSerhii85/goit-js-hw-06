fd4f7c43be0d52fc067be710ba5a3b2dd1b32cc9
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-04 13:45:58 +0200

    Make task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formInputs = {};
+
+    const {email, password} = event.currentTarget.elements;
+
+    if (!email.value) {
+        alert('Fill in email form fields');
+    } else if (!password.value) {
+        alert('Fill in password form fields');
+    } else 
+            formInputs.password = password.value;
+            formInputs.email = email.value;
+
+    console.log(formInputs);
+    form.reset();
+};
