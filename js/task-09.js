741366d6e63659df463eaff0c9646a356b7076e8
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-04 14:34:53 +0200

    Make task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+ const bodyEl = document.querySelector('body');
+ const buttonEl = document.querySelector('.change-color');
+ const colorEl = document.querySelector('.color')
+
+ buttonEl.addEventListener('click', changeColor);
+
+ function changeColor () {
+  colorEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+ };
