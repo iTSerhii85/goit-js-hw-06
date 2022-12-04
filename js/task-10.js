279876e90de8770b282ea0e832c1c3f3671af609
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-04 16:32:32 +0200

    Make task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const controlsEl = document.querySelector('#controls');
+const boxes = document.querySelector('#boxes');
+const create = document.querySelector('button[data-create]');
+const destroy = document.querySelector('button[data-destroy]');
+const input = controlsEl.querySelector('input');
+
+create.addEventListener('click', getAmount);
+destroy.addEventListener('click', destroyBoxes);
+
+function getAmount() {
+  let amount = input.value;
+  createBoxes(amount);
+};
+
+function createBoxes(amount) {
+  let basicSize = 30;
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i+=1) {
+    let size = basicSize + i * 10;
+    let div = document.createElement("div");
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = `${getRandomHexColor()}`;
+  
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
