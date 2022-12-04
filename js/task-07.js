6c5a7002e17e64dc179cdbe2724e2c4ea5006c10
Сергей <109918837+iTSerhii85@users.noreply.github.com>
2022-12-04 16:39:49 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,9 +2,9 @@ const text = document.querySelector('#text');
 const inputRange = document.querySelector('#font-size-control');
 
 // ! Вариант 1
-// inputRange.addEventListener('input', 
-// (event) => text.style.fontSize = `${event.currentTarget.value}px`);
+inputRange.addEventListener('input', 
+(event) => text.style.fontSize = `${event.currentTarget.value}px`);
 
 // ! Вариант 2
-inputRange.addEventListener('input', 
-() => text.style.fontSize = `${inputRange.value}px`);
+// inputRange.addEventListener('input', 
+// () => text.style.fontSize = `${inputRange.value}px`);
